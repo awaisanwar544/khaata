2936e06a47f4228b13d1e3ce09163226a7bfb06e
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-29 18:07:36 +0500

    Add user model Install and setup devise

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,13 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get home_index_url
-    assert_response :success
-  end
-
-  test "should get splash" do
-    get home_splash_url
     assert_response :success
   end
 end
