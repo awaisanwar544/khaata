7dc70e9d0b54d69ec675e3d30853ab6076454d72
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-30 21:34:55 +0500

    Tests for requests

--- spec/requests/payment_spec.rb
+++ spec/requests/payment_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'Categories', type: :request do
+RSpec.describe 'Payments', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/categories'
+      get '/categories/:category_id/payments'
+      # Expected to be found but inaccesible due to authorization
       expect(response).to have_http_status(:found)
     end
   end
 
   describe 'POST /index' do
     it 'returns http success' do
-      post '/categories'
+      post '/categories/:category_id/payments'
       # Expected to be found but inaccesible due to authorization
       expect(response).to have_http_status(:found)
     end
