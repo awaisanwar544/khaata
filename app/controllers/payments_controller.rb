0305e6abc4286ca4907e29ccf027d620ceec3098
awaisanwar544 <awaisanwar544@gmail.com>
2022-06-30 15:00:14 +0500

    Add transaction pages

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,24 @@
 class PaymentsController < ApplicationController
-  def index; end
+  def index
+    @payments = current_user.categories.find(params[:category_id]).payments
+  end
+
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+    @payment.user_id = current_user.id
+    @payment.category_id = params[:category_id]
+
+    if @payment.save
+      redirect_to category_payments_path(@payment), notice: 'Transaction Added'
+    else
+      @errors = @payment.errors.full_messages
+      render :new
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :payments
+  resources :categories do
+    resources :payments
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
